7e82ab09175b7057257a5ebcc9b8d7053430d80f
DEYO <mdeyo@mit.edu>
2015-10-06 09:55:18 -0400

    addPedestrian multiple

--- ConnectionToDatabase.py
+++ ConnectionToDatabase.py
@@ -1,6 +1,7 @@
 __author__ = 'mdeyo'
 
 from couchdb import Server,Database
+import time
 
 #server = Server('http://localhost:5984/') #connects to remote server
 database = None
@@ -13,17 +14,21 @@ server = Server('http://mdeyo.iriscouch.com:5984/') #connects to remote server
 class ConnectionToDatabase(object):
 
     def __init__(self, name='server'):
+        self.time_start = time.clock()
         #self.server = Server('http://'+laptop_address+':5984/') #connects to remote server
         self.server = Server('http://mdeyo.iriscouch.com:5984/') #connects to remote server
         self._name = name
-        self.ped_database = self.openDB("ped_db")
-        self.vehicle_database = self.openDB("vehicle_db")
+        self.ped_database = self.open_db("ped_db")
+        self.vehicle_database = self.open_db("vehicle_db")
         print("connected!")
 
     def get_server(self):
         return self.server
 
-    def openDB(self,name):
+    def get_start_time(self):
+        return self.time_start
+
+    def open_db(self,name):
         """Opens up connection to db of name and returns  object"""
         if(server.__contains__(name)):
             database = server.__getitem__(name)
@@ -33,7 +38,7 @@ class ConnectionToDatabase(object):
 
         return database
 
-    def openDoc(self,name):
+    def open_doc(self,name):
         """Opens up connection to db of name and returns  object"""
         if(self.database.__contains__(name)):
             doc = server.__getitem__(name)
@@ -61,12 +66,32 @@ class ConnectionToDatabase(object):
             self.ped_database.save(ped_doc)
         return ped_doc
 
+    def add_pedestrian_data(self,id,list_of_points):
+        """Adds pedestrian data to the database"""
+        # add pedestrian data with "id", timestamp, and pose
+        ped_data_doc = "pedestrians"
+        ped_doc = None
+        if(self.ped_database.__contains__(id)):
+            # if pedestrian id already exists - appends to history list and updates time
+            ped_doc = self.ped_database.__getitem__(id)
+            history = ped_doc['history']
+            history.append(list_of_points)
+            ped_doc['history'] = history
+            ped_doc['recent_time'] = time
+            self.ped_database.save(ped_doc)
+        else: # else, make new document with pedestrian id
+            ped_doc = {'_id': id, 'history': [list_of_points], 'recent_time':time}
+            self.ped_database.save(ped_doc)
+        return ped_doc
+
     def print_pedestrian_doc(self,ped_doc):
-        print("Print Pedestrian Document: ID="+ped_doc._id)
-        print('History:',)
+        print("Print Pedestrian Document: ID="+ped_doc.id)
+        print("Most Recent Time: "+ped_doc['recent_time'])
+
+        print'History:'
         history_list = ped_doc['history']
         for i in history_list:
-            print("Timestamp:"+i[0],"Position"+i[1])
+            print("Timestamp:"+str(i[0]),"Position:"+str(i[1]))
 
 ####running test code####
 
@@ -84,7 +109,9 @@ server = db.get_server()
 
 #print(db.openDB("ped_db").__getitem__("5600"))
 
-db.print_pedestrian_doc(db.openDB("ped_db").__getitem__("5600"))
+db.print_pedestrian_doc(db.open_db("ped_db").__getitem__("5600"))
 
-#print('document',db.openDB("new_db").__getitem__("first_doc"))
+#print('document',db.open_db("new_db").__getitem__("first_doc"))
+
+print("time elapsed",str(time.clock()-db.get_start_time()))
 
